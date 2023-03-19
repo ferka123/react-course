b55f8b056aeb26f3208ae9680726e3367254f275
ferka123 <ferka911@gmail.com>
2023-03-19 20:56:10 +0200

    feat: search functionality

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,21 +3,42 @@ import { Component } from 'react';
 import { data } from '../fakedata/fakedata';
 import './styles/home.scss';
 
-export default class Home extends Component<{ setCurrentPage: (name: string) => void }> {
+interface Props {
+  setCurrentPage: (name: string) => void;
+}
+
+interface State {
+  searchValue: string;
+}
+
+export default class Home extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      searchValue: '',
+    };
+  }
   componentDidMount(): void {
     this.props.setCurrentPage('Home');
+    this.setState({ searchValue: localStorage.getItem('searchValue') ?? '' });
   }
+  handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ searchValue: e.target.value });
+    localStorage.setItem('searchValue', e.target.value);
+  };
   render() {
     return (
       <>
         <div className="search-form">
-          <input type="text" />
+          <input type="text" value={this.state.searchValue} onChange={this.handleInputChange} />
           <button>Search</button>
         </div>
         <div className="cards">
-          {data.map((cardData) => (
-            <Card key={cardData.id} data={cardData} />
-          ))}
+          {data
+            .filter((cardData) => cardData.name.toLowerCase().includes(this.state.searchValue))
+            .map((cardData) => (
+              <Card key={cardData.id} data={cardData} />
+            ))}
         </div>
       </>
     );
