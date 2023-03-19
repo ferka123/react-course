e379f1214d5184b027d41f5d5d6c935dc8d51cbb
ferka123 <ferka911@gmail.com>
2023-03-19 20:29:10 +0200

    feat: cards from fakedata

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,25 @@
+import Card from '../components/Card';
 import { Component } from 'react';
+import { data } from '../fakedata/fakedata';
+import './styles/home.scss';
 
 export default class Home extends Component<{ setCurrentPage: (name: string) => void }> {
   componentDidMount(): void {
     this.props.setCurrentPage('Home');
   }
   render() {
-    return <div>Home</div>;
+    return (
+      <>
+        <div className="search-form">
+          <input type="text" />
+          <button>Search</button>
+        </div>
+        <div className="cards">
+          {data.map((cardData) => (
+            <Card key={cardData.id} data={cardData} />
+          ))}
+        </div>
+      </>
+    );
   }
 }
