b6976cc3c841d7c9ddae16afaee04785a0a2f435
ferka123 <ferka911@gmail.com>
2023-04-12 18:06:51 +0300

    feat: rtk queries for search and getById products

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,13 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import searchReducer from './features/searchSlice';
 import formReducer from './features/formSlice';
+import { api } from './api';
 
 export const store = configureStore({
   reducer: {
+    [api.reducerPath]: api.reducer,
     searchState: searchReducer,
     formState: formReducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware({}),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({}).concat([api.middleware]),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
---
+++ src/types/api.ts
@@ -0,0 +1,9 @@
+export interface Product {
+  id: number;
+  title: string;
+  image: string;
+  date: string;
+  rules: boolean;
+  condition: string;
+  category: string;
+}
