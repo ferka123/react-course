97cd9869ad31188d3c19ae819d897a73b7065fef
ferka123 <ferka911@gmail.com>
2023-04-12 18:13:22 +0300

    feat: replacing queries with rtk queries

--- src/components/cardlist/CardList.tsx
+++ src/components/cardlist/CardList.tsx
@@ -1,36 +1,16 @@
-import { getProducts } from '../../api/service';
 import Card from '../card/Card';
-import { useEffect, useState } from 'react';
-import { Product } from '../../api/types';
 import Spinner from '../loaders/Spinner';
 import classes from './cardList.module.scss';
 import { useAppSelector } from '../../redux/store';
+import { useSearchProductQuery } from '../../redux/api';
 
 const CardList = () => {
   const searchQuery = useAppSelector((state) => state.searchState.searchQuery);
-  const [data, setData] = useState<Product[]>([]);
-  const [isError, setIsError] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        setIsError(false);
-        setIsLoading(true);
-        setData(await getProducts(searchQuery));
-      } catch {
-        setIsError(true);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    fetchData();
-  }, [searchQuery]);
+  const { data, isError, isFetching } = useSearchProductQuery(searchQuery);
 
   if (isError) return <div role={'alert'}>Error</div>;
-  if (isLoading) return <Spinner color="#fff" className={classes.loader} />;
-  if (!data.length) return <div>No products</div>;
+  if (isFetching) return <Spinner color="#fff" className={classes.loader} />;
+  if (!data?.length) return <div>No products</div>;
   return (
     <>
       <div className={classes.cards}>
